9c38e91f03eb823b44768b384d679ae4bb27155d
Jostein Leira <jostein.leira@computas.com>
2020-12-02 17:27:16 +0100

    Day two, part two.

--- day2/main.py
+++ day2/main.py
@@ -4,8 +4,7 @@ Count number of valid passwords
 from itertools import filterfalse
 
 
-def main():
-    # Use a breakpoint in the code line below to debug your script.
+def part1():
     with open('passwords.txt') as f:
         passwords = [data.strip().split(' ') for data in f]
 
@@ -16,5 +15,24 @@ def main():
     print('Number of valid passwords:', len(list(result)))
 
 
+def part2():
+    with open('passwords.txt') as f:
+        passwords = [data.strip().split(' ') for data in f]
+
+    print(list(passwords))
+
+    # pos1 = int(x[0].split('-')[0])-1
+    # pos2 = int(x[0].split('-')[1])-1
+    # pwd = x[2]
+    # chr = x[1][0]
+    # ^ = bitwise xor
+    # bool(pwd[pos1]==chr) ^ bool(pwd[pos2]==chr)
+    result = list(filterfalse(lambda x: not(bool(x[2][int(x[0].split('-')[0])-1] == x[1][0]) ^ bool(x[2][int(x[0].split('-')[1])-1] == x[1][0])), passwords))
+
+    print(result)
+    print('Number of valid passwords:', len(result))
+
+
 if __name__ == '__main__':
-    main()
+    # part1()
+    part2()
